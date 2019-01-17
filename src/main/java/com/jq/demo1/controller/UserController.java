80b3fcf33f5d4d65dd104e55b3d53e8f52ae2fb4
jiaqi <jiaqi@zenmen.com>
2019-01-17 19:39:48 +0800

    otp接口完善，待短信接口接入

--- src/main/java/com/jq/demo1/controller/UserController.java
+++ src/main/java/com/jq/demo1/controller/UserController.java
@@ -12,24 +12,38 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @Author: jq
  * @Date: 2019/1/17 11:38
+ * CrossOrigin注解可以处理前后端分离的时候的跨域请求错误
  */
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private HttpServletRequest request;
 
     @RequestMapping("otp")
     @ResponseBody
     public CommonReturnType getOtp(@RequestParam("telphone") String telphone) {
         //按照一定的规则生成otp验证码
+        Random random = new Random();
+        int v = (int)(random.nextDouble() * 1000000 - 1);
+
+        String s = String.valueOf(v);
+        if (s.length() < 6) {
+            s += '0';
+        }
         //将otp验证码同对应用户的手机号关联
-        //将otp验证码通过短信通道发送给用户
+        request.setAttribute(telphone, s);
+        //将otp验证码通过短信通道发送给用户(短信接口没有，我是个弟弟)
+        System.out.println(request.getAttribute(telphone));
         return CommonReturnType.create(null);
     }
 
